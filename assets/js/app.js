507eec3bc8c190a86692f02d7c9e621f7a0f706b
unknown <zz240614@gmail.com>
2025-03-07 23:44:30 +0000

    fetch and desplay Education  data dynamic from json file

--- assets/js/app.js
+++ assets/js/app.js
@@ -24,3 +24,31 @@ fetch('experience.json')
      });
 })
 .catch(error => console.error("Error fetching data expriernce:", error));
+
+// fetch Education
+
+fetch('education.json')
+  .then(res => res.json())
+  .then(data => {
+    const educationContainer = document.getElementById("education");
+    data.forEach(education => {
+      const educationDiv = document.createElement("div");
+      educationDiv.classList.add("border-b", "border-solid", "mb-6", "pb-6");
+
+      educationDiv.innerHTML = `
+        <div class="job-title-container md:flex-col flex-row flex justify-between text-lg mb-3">
+          <div>
+            <div class="job-company font-bold leading-[1.2]">${education.school}</div>
+            <div class="job-title">${education.job_title}</div>
+          </div>
+          <div>
+            ${education.date}
+          </div>
+        </div>
+        <p>${education.description}</p>
+      `;
+
+      educationContainer.appendChild(educationDiv);
+    });
+  })
+  .catch(error => console.error("Error fetching data:", error));
